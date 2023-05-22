13413355d737b9bfbedc0a1a63903d9b827ce5e9
Memo <2415782224@qq.com>
2023-05-22 12:32:01 +0800

    HelloWorld.c 案例

---
+++ day01/HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+//    return 0;    // 这行代码可以不用写
+}
--- day01/demo01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("钟超棋真帅!\n");
-    return 0;
-}
