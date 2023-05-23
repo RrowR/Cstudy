8724994493335b792e6c6063d0cbedcc6d7b0ae8
Memo <2415782224@qq.com>
2023-05-23 22:58:38 +0800

    加减乘除

---
+++ Cbasic/002Printf/002_zuoye.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    int b = 9;
+
+    printf("两数之和为%d\n", a + b);
+    printf("两数之差为%d\n", a - b);
+    printf("两数之积为%d\n", a * b);
+    printf("两数相除为%f\n", (float) a / b);
+
+}
