a4669cf72bb872a3dfb1d7073512de620f6673a4
Memo <2415782224@qq.com>
2023-05-25 01:13:40 +0800

    字符转换

---
+++ Cbasic/003Scanf/003_Input_test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+    char d = 'd';
+    char A = 'A';
+    char B = 'B';
+    char C = 'C';
+    char D = 'D';
+
+    printf("a=%d,b=%d,c=%d,d=%d",a,b,c,d);
+    printf("A=%d,B=%d,C=%d,D=%d",A,B,C,D);
+
+
+}
