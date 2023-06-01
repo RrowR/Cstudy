670b2f413f7bf1ed8359bcfe80443fb79c040f37
Memo <2415782224@qq.com>
2023-06-01 22:24:42 +0800

    加减乘除

---
+++ Cbasic/003Scanf/005Zuoye1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * 加减乘除
+ * @return
+ */
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    printf("您输出的加法的和是%d\n",a+b);
+    printf("您输出的乘法的和是%d\n",a-b);
+    printf("您输出的减法的和是%d\n",a*b);
+    printf("您输出的除法的和是%d\n",a/b);
+}
