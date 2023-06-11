2196765872897e378ab4c8d58a124c2084eb5ae2
Memo <2415782224@qq.com>
2023-06-12 04:55:00 +0800

    大小写转换

---
+++ Cbasic/004流程控制/004作业.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * 将输入的字母转换成小写字母,并输出
+ * @return
+ */
+int main() {
+
+    char cdata;
+
+    printf("请输入一个字符\n");
+    scanf("%c", &cdata);
+
+//    判断是否是大写字母
+    if (cdata >= 97 && cdata <= 122) {
+        printf("%c\n", cdata);
+    } else {
+        printf("%c", cdata + 32);
+    }
+
+    printf("\ndone! \n");
+    return 0;
+
+}
