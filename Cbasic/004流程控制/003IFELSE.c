dca779f29415e8ce31daa2dabcb5e48e7e42bf0d
Memo <2415782224@qq.com>
2023-06-12 03:45:34 +0800

    IF/ELSE的使用

---
+++ Cbasic/004流程控制/003IFELSE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * IF/ELSE的使用
+ * @return
+ */
+int main()
+{
+
+    int a;
+
+    printf("请输入0或者1 \n");
+    scanf("%d",&a);
+
+    if (a) {
+        printf("成立\n");
+    } else{
+        printf("不成立\n");
+    }
+
+
+}
