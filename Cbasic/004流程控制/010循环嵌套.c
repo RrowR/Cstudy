24cc4576af02c24beb2cc9e2e4d70eecb2c25f6a
Memo <2415782224@qq.com>
2023-06-15 23:54:26 +0800

    循环嵌套2层for

---
+++ Cbasic/004流程控制/010循环嵌套.c
@@ -0,0 +1,19 @@
+/**
+ * @author: Rrow
+ * @date: 2023/6/15 23:49
+ * Description: 
+ */
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= 5; ++j) {
+            printf("%d\t", i * j);
+        }
+        printf("\n");
+    }
+
+
+
+}
