c1db5d97fe87f925aaaa4337b56f104445415671
Memo <2415782224@qq.com>
2023-06-16 00:03:39 +0800

    数组

---
+++ Cbasic/005数组/001数组.c
@@ -0,0 +1,26 @@
+/**
+ * @author: Rrow
+ * @date: 2023/6/16 0:00
+ * Description: 
+ */
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    arr[0] = 100;
+    arr[1] = 100;
+    arr[2] = 200;
+    arr[3] = 300;
+    arr[4] = 400;
+    arr[5] = 500;
+    arr[6] = 600;
+    arr[7] = 700;
+    arr[8] = 800;
+    arr[9] = 900;
+    puts("数组初始化完毕");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\n", arr[i]);
+    }
+    puts("完毕");
+}
