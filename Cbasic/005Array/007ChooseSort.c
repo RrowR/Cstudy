6294ea2799bfb6ae8ae7e3705cef77edeb5bd53e
Memo <2415782224@qq.com>
2023-06-16 02:37:25 +0800

    选择排序没写完

---
+++ Cbasic/005Array/007ChooseSort.c
@@ -0,0 +1,26 @@
+/**
+ * @author: Rrow
+ * @date: 2023/6/16 1:03
+ * Description: 选择排序
+ */
+#include <stdio.h>
+int main()
+{
+//    int arr[] = {1, 5, 4, 8, 3, 2};
+//
+//    int size = sizeof(arr) / sizeof(arr[0]);
+//
+//    for (int i = 0; i < size - 1; ++i) {
+//        for (int j = i; j < size - 1; ++j) {
+//            if (arr[i] > arr[j]) {
+//                arr[i] = arr[i] + arr[j];
+//                arr[j] = arr[i] - arr[j];
+//                arr[j] = arr[i] - arr[j];
+//            }
+//        }
+//    }
+//
+//    for (int i = 0; i < size; ++i) {
+//        printf("%d ", arr[i]);
+//    }
+}
