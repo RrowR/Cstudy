50836dc5d8d32a928d88eeca683c73c5ff4f6829
Memo <2415782224@qq.com>
2023-06-15 23:48:17 +0800

    For循环

---
+++ Cbasic/004流程控制/009For循环.c
@@ -0,0 +1,13 @@
+/**
+ * @author: Rrow
+ * @date: 2023/6/15 23:15
+ * Description: 
+ */
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 100; ++i) {
+        printf("love you %d times \n",i);
+    }
+}
