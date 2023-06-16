dbc5cb555a8c8d96c370ce6b4047ceb99194f916
Memo <2415782224@qq.com>
2023-06-16 23:43:33 +0800

    选择排序

--- Cbasic/005Array/007ChooseSort.c
+++ Cbasic/005Array/007ChooseSort.c
@@ -1,26 +1,30 @@
 /**
  * @author: Rrow
  * @date: 2023/6/16 1:03
- * Description: 选择排序
+ * Description: 选择排序,就是每次进行比较,大于的就交换
  */
 #include <stdio.h>
-int main()
-{
-//    int arr[] = {1, 5, 4, 8, 3, 2};
-//
-//    int size = sizeof(arr) / sizeof(arr[0]);
-//
-//    for (int i = 0; i < size - 1; ++i) {
-//        for (int j = i; j < size - 1; ++j) {
-//            if (arr[i] > arr[j]) {
-//                arr[i] = arr[i] + arr[j];
-//                arr[j] = arr[i] - arr[j];
-//                arr[j] = arr[i] - arr[j];
-//            }
-//        }
-//    }
-//
-//    for (int i = 0; i < size; ++i) {
-//        printf("%d ", arr[i]);
-//    }
+
+int main() {
+    int arr[] = {3, 5, 2, 4, 7, 11, 23, 6};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int temp;
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for (int k = 0; k < size; ++k) {
+        printf("%d ", arr[k]);
+    }
+
+
 }
