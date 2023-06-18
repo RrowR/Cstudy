a1c0c50155978b848c328cae9828e85c44f2e2fe
Memo <2415782224@qq.com>
2023-06-19 00:27:02 +0800

    编程案例: 有两个班的同学，分别是10个人和5个人，分别求这两个班的平均分

---
+++ Cbasic/006Fx/006AvgScore.c
@@ -0,0 +1,29 @@
+/**
+ * @author: Rrow
+ * @date: 2023/6/18 23:15
+ * Description: 编程案例: 有两个班的同学，分别是10个人和5个人，分别求这两个班的平均分
+ */
+#include <stdio.h>
+
+double calculateAvg(double array[],int lenth)
+{
+    double sum;
+    for (int i = 0; i < lenth; i++) {
+        sum += array[i];
+    }
+
+    return sum/lenth;
+}
+
+int main()
+{
+    double avg10;
+    double avg5;
+    double class10[10] = {11.1, 22.2, 33.3, 44.4, 55.5, 66.6, 77.7, 88.8, 99.9, 100.0};
+    double class5[5] = {22.2, 22.2, 22.2, 22.2, 22.2};
+    avg10 = calculateAvg(class10, sizeof(class10)/sizeof(class10[0]));
+    avg5 = calculateAvg(class5,sizeof(class5)/sizeof(class5[0]));
+//    这里要用浮点数
+    printf("avg10:%f\n", avg10);
+    printf("avg5:%f\n", avg5);
+}
