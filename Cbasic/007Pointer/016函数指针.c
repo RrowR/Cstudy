abc225782efe17254c3cdaff0aceae537acde551
MeroMemo <1043526080@qq.com>
2023-06-29 15:10:44 +0800

    函数指针的定义和使用

--- Cbasic/007Pointer/016函数指针.c
+++ Cbasic/007Pointer/016函数指针.c
@@ -5,9 +5,22 @@
  */
 #include <stdio.h>
 
+void toPrintHelloWorld(){
+    printf("Hello world \n");
+}
 
+void toPrintInput(int data){
+    printf("data = %d\n",data);
+}
 
 int main()
 {
-
+//    定义一个函数指针
+    void (*p)();
+    void (*p2)();
+//    指针指向方法
+    p = toPrintHelloWorld;
+    (*p)();
+    p2 = toPrintInput;
+    p2(2);
 }
